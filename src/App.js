3086ff9354afe38654d1ea64c3203b25ba9be52d
LJS22 <LJS22@users.noreply.github.com>
2020-07-31 14:24:38 +0100

    improved image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
-
 import RandomFunction from "./components/Luke";
-
 import Mordechai from "./components/Mordechai";
-
 import Niall from "./components/Niall";
-
-import RandomFunction from "./components/Luke";
-
 import Picture from "./components/Picture";
-
-
 import { Gareth } from "./components/Gareth"
-
-
-import Picture from "./components/Picture";
 import Sara from "./components/Sara";
-
 import myName from './components/Adam';
 
 
@@ -28,16 +15,9 @@ class App extends Component {
     dan: "Dan Krishnan",
     mordechai: "Mordechai Weinstock",
     luke: "Luke Stobbart",
-
-
     nat: "Natalia",
-
-    gareth: "Gareth Scott"
-
-    adam: 'Adam Horscraft'
-
-
-
+    gareth: "Gareth Scott",
+    adam: 'Adam Horscraft',
     niall: "Niall",
     nat: "Natalia",
     sara: "Sara",
@@ -51,23 +31,13 @@ class App extends Component {
       <div className="App">
         <Card name={this.state.dan} />
         <RandomFunction luke={luke} />
-
-
         <Card name={this.state.adam} />
-
-
-
-
-
-
         <Niall name={this.state.niall} />
         <Picture />
         <Card name={this.state.dan} />
         <Card name={this.state.nat} />
         <Sara sara={sara} pic={pic} />
         <Mordechai name={this.state.mordechai} />
-
-
       </div>
     );
   }
