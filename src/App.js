3bf0dc17c329dfbb68e478a61d422a7b46cfd92e
MrRawrgers <niall.w.rogers@gmail.com>
2020-07-31 11:32:54 +0100

    added Niall.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
+import Niall from "./components/Niall";
 
 class App extends Component {
   state = {
     dan: "Dan Krishnan",
+    niall: "Niall Rogers"
   };
 
   render() {
     return (
       <div className="App">
         <Card name={this.state.dan} />
+        <Niall name={this.state.niall} />
       </div>
     );
   }
