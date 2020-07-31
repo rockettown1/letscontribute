60bc9423b9a95a3db3d1738bf7e43e02dc66c6ee
katiehughes <katie,m.hughes@hotmail.co.uk>
2020-07-31 11:37:30 +0100

    Added new card with my name inside - Katie

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
+import KatieCard from "./components/KatieCard"
 
 class App extends Component {
   state = {
     dan: "Dan Krishnan",
+    katie: "Katie Hughes"
   };
 
   render() {
     return (
       <div className="App">
         <Card name={this.state.dan} />
+        <KatieCard name={this.state.katie} />
       </div>
     );
   }
---
+++ src/components/KatieCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const KatieCard = ({ name }) => {
+    return (
+        <div>
+            <h1>Hi I'm {name}</h1>
+        </div>
+    );
+};
+
+export default KatieCard;
