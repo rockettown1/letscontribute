f6dc5b54af525ff7e0ba06c59260ce2e1e01b468
Mordechai Weinstock <gefenemail@gmail.com>
2020-07-31 11:39:54 +0100

    added Mordechai component with a corresponding css file

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
+import Mordechai from "./components/Mordechai";
 
 class App extends Component {
   state = {
     dan: "Dan Krishnan",
+    mordechai: "Mordechai Weinstock"
   };
 
   render() {
     return (
       <div className="App">
         <Card name={this.state.dan} />
+        <Mordechai name={this.state.mordechai} />
       </div>
     );
   }
---
+++ src/components/Mordechai.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Mordechai.css';
+
+const Mordechai = ({ name }) => {
+    return (
+        <div>
+            <h2 className="name">I am {name}</h2>
+        </div>
+    )
+}
+
+export default Mordechai;
