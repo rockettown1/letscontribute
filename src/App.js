9cf0056ac978c4fd471dbfc92d9099cafa968584
LJS22 <LJS22@users.noreply.github.com>
2020-07-31 11:15:19 +0100

    added new feature

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
+import RandomFunction from "./components/Luke";
 
 class App extends Component {
   state = {
     dan: "Dan Krishnan",
+    luke: "Luke Stobbart"
   };
 
   render() {
+    const { luke } = this.state
     return (
       <div className="App">
         <Card name={this.state.dan} />
+        <RandomFunction luke={luke} />
       </div>
     );
   }
