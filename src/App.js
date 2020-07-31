50a3c0204b567c4c5e87d8ec1c26063d710c6fb3
sarafotros <sarafotros@gmail.com>
2020-07-31 14:29:22 +0100

    time added to header

--- src/App.js
+++ src/App.js
@@ -1,76 +1,57 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Card from "./components/Card";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card';
 
-import RandomFunction from "./components/Luke";
+import Mordechai from './components/Mordechai';
 
-import Mordechai from "./components/Mordechai";
+import Niall from './components/Niall';
 
-import Niall from "./components/Niall";
+import RandomFunction from './components/Luke';
 
-import RandomFunction from "./components/Luke";
+import { Gareth } from './components/Gareth';
 
-import Picture from "./components/Picture";
-
-
-import { Gareth } from "./components/Gareth"
-
-
-import Picture from "./components/Picture";
-import Sara from "./components/Sara";
+import Picture from './components/Picture';
+import Sara from './components/Sara';
 
 import myName from './components/Adam';
-
+import Time from './components/Time';
 
 class App extends Component {
-  state = {
-    dan: "Dan Krishnan",
-    mordechai: "Mordechai Weinstock",
-    luke: "Luke Stobbart",
+	state = {
+		dan: 'Dan Krishnan',
+		mordechai: 'Mordechai Weinstock',
+		luke: 'Luke Stobbart',
 
+		nat: 'Natalia',
 
-    nat: "Natalia",
+		gareth: 'Gareth Scott',
 
-    gareth: "Gareth Scott"
+		niall: 'Niall',
+		nat: 'Natalia',
+		sara: 'Sara',
+		pic: 'https://picsum.photos/200/300',
+	};
 
-    adam: 'Adam Horscraft'
+	render() {
+		const { luke, sara, pic } = this.state;
+		return (
+			<div className="App">
+				<Time />
+				<Card name={this.state.dan} />
+				<RandomFunction luke={luke} />
 
+				<Card name={this.state.adam} />
 
-
-    niall: "Niall",
-    nat: "Natalia",
-    sara: "Sara",
-    pic: "https://picsum.photos/200/300",
-
-  };
-
-  render() {
-    const { luke, sara, pic } = this.state;
-    return (
-      <div className="App">
-        <Card name={this.state.dan} />
-        <RandomFunction luke={luke} />
-
-
-        <Card name={this.state.adam} />
-
-
-
-
-
-
-        <Niall name={this.state.niall} />
-        <Picture />
-        <Card name={this.state.dan} />
-        <Card name={this.state.nat} />
-        <Sara sara={sara} pic={pic} />
-        <Mordechai name={this.state.mordechai} />
-
-
-      </div>
-    );
-  }
+				<Niall name={this.state.niall} />
+				<Picture />
+				<Card name={this.state.dan} />
+				<Card name={this.state.nat} />
+				<Sara sara={sara} pic={pic} />
+				<Mordechai name={this.state.mordechai} />
+			</div>
+		);
+	}
 }
 
 export default App;
