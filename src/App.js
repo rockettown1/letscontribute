644740881099e22cc2dff1fa9b8f656bcc5ca4d5
Mordechai Weinstock <gefenemail@gmail.com>
2020-07-31 15:07:14 +0100

    added some NON-WORKING code to Mordechai component

--- src/App.js
+++ src/App.js
@@ -2,52 +2,33 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
-
 import RandomFunction from "./components/Luke";
-
 import Mordechai from "./components/Mordechai";
-
 import Niall from "./components/Niall";
-
-import RandomFunction from "./components/Luke";
-
 import Picture from "./components/Picture";
-
-import { Gareth } from "./components/Gareth"
-
-
-import { Gareth } from "./components/Gareth"
-
-
-import Picture from "./components/Picture";
+import { Gareth } from "./components/Gareth";
 import Sara from "./components/Sara";
-
 import myName from './components/Adam';
-
 
 class App extends Component {
   state = {
     dan: "Dan Krishnan",
     mordechai: "Mordechai Weinstock",
+    classNames: {name: "red"},
     luke: "Luke Stobbart",
-
-
     nat: "Natalia",
-
     nat: "Natalia",
-
     gareth: "Gareth Scott",
-
     adam: 'Adam Horscraft',
-
-
-
     niall: "Niall",
     nat: "Natalia",
     sara: "Sara",
     pic: "https://picsum.photos/200/300",
+  }
 
-  };
+  classNameHandler = () => {
+    this.state.classNames === "red" ? this.setState({name: "green"}) : this.setState({name: "red"})
+  }
 
   render() {
     const { luke, sara, pic } = this.state;
@@ -55,29 +36,18 @@ class App extends Component {
       <div className="App">
         <Card name={this.state.dan} />
         <RandomFunction luke={luke} />
-
         <Picture />
         <Card name={this.state.dan} />
         <Card name={this.state.nat} />
-
-
-
-
         <Card name={this.state.adam} />
-
-
-
-
-
-
         <Niall name={this.state.niall} />
         <Picture />
         <Card name={this.state.dan} />
         <Card name={this.state.nat} />
         <Sara sara={sara} pic={pic} />
-        <Mordechai name={this.state.mordechai} />
-        <Gareth gareth={this.state.gareth} />
-      </div >
+        <Mordechai name={this.state.mordechai} classNameHandler={this.classNameHandler} className={this.state.classNames.name}/>
+        <Gareth gareth={this.state.gareth}/>
+      </div>
     );
   }
 }
--- src/components/Mordechai.js
+++ src/components/Mordechai.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Mordechai.css';
 
-const Mordechai = ({ name }) => {
+const Mordechai = ({ name, classNameHandler, className }) => {
     return (
         <div>
-            <h2 className="name">I am {name}</h2>
+            <h2 className={className}>{name} is trying to put it all together...</h2>
+            <button onClick={classNameHandler}>click to change Mordechai colour</button>
         </div>
     )
 }
