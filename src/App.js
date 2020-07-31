e8a349ab2fca5bc28c5acf797bd5d584e692db6e
Dan Krishnan <dan@wearecodenation.com>
2020-07-31 10:59:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    dan: "Dan Krishnan",
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Card name={this.state.dan} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = ({ name }) => {
+  return (
+    <div>
+      <h1> Hi I'm {name}</h1>
+    </div>
+  );
+};
+
+export default Card;
